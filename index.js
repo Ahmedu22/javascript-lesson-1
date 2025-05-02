13ff969fd2bcbef1c77284711524d2dd3363e448
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-02 12:42:13 +0300

    java-1st-lesson

---
+++ index.js
@@ -0,0 +1,24 @@
+// console.log("Ahmed");
+// alert("this an alert option");
+// data types
+// 1. strings
+// 2.Number
+// 3.boolean
+// console.log(typeof "ahmed");
+// console.log( typeof 123);
+// console.log(typeof true)
+
+// prompt("whats your name")
+var name = "ahmed";
+console.log(name);
+var name = "zubeir";
+console.log(name);
+var age = "25";
+console.log(age)
+
+// prompt
+var name = prompt ("whats your name")
+console.log(name)
+
+var age = prompt("whats your age")
+console.log(age)
